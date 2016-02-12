77cb54dcb492eb57547f6574069072da72d74f2d
Jonathan Bernesser <jbern16@gmail.com>
2016-02-12 14:00:13 -0700

    made swap_numbers method for bs

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,23 +1,29 @@
 class BubbleSort
 
-def bubble_sort(array)
+def bubble_sort(array_to_get_sorted)
   array_size = array.length
   until sorted?(swaps)
     swaps = 0
     (array_size-1).times do |index|
-      if array[index] > array[index+1]
-        array[index], array[index+1] = array[index+1], array[index]
-        swaps += 1
-      end
+      swap_numbers(array)
       sorted?(swaps)
     end
   end
-  array
+  array_to_get_sorted
 end
+
 
 def sorted?(swaps)
   swaps > 0 ? sorted = false : sorted = true
 end
+
+def swap_numbers(array)
+  if array[index] > array[index+1]
+    array[index], array[index+1] = array[index+1], array[index]
+    swaps += 1
+  end
+end
+
 
 
 end
