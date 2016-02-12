dad894064b4db7174b7efc800faeab09275fb37a
Jonathan Bernesser <jbern16@gmail.com>
2016-02-12 15:50:16 -0700

    bubble sort working. need more tests

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,28 +1,29 @@
 class BubbleSort
 
-def bubble_sort(array_to_get_sorted)
-  array_size = array.length
-  until sorted?(swaps)
-    swaps = 0
-    (array_size-1).times do |index|
-      swap_numbers(array)
+  def bubble_sort(array)
+    array_size = array.length
+    loop do
+      swaps = false
+      swap_numbers(array, swaps, array_size)
       sorted?(swaps)
+      break if sorted?(swaps)
+     end
+    array
+  end
+
+  def sorted?(swaps)
+    swaps == false
+  end
+
+  def swap_numbers(array, swaps, array_size)
+    (array_size - 1).times do |index|
+     if array[index] > array[index+1]
+       array[index], array[index+1] = array[index+1], array[index]
+       swaps = true
+       bubble_sort(array)
+     end
     end
   end
-  array_to_get_sorted
-end
-
-
-def sorted?(swaps)
-  swaps > 0 ? sorted = false : sorted = true
-end
-
-def swap_numbers(array)
-  if array[index] > array[index+1]
-    array[index], array[index+1] = array[index+1], array[index]
-    swaps += 1
-  end
-end
 
 
 
--- bubble_sort_test.rb
+++ bubble_sort_test.rb
@@ -1,21 +1,24 @@
-require_relative 'bubble_sort'
-
 gem 'minitest', '>= 5.2'
 require 'minitest/autorun'
 require 'minitest/pride'
-require_relative 'binary_search_tree'
-require_relative 'node'
+require_relative 'bubble_sort'
 
 class BubbleSortTest < Minitest::Test
   attr_accessor :bs
+
   def setup
     @bs = BubbleSort.new
   end
 
-  def check_if_bubble_sort_works_on_simple_array
-  skip
-  assert_equal, 
+  def test_check_if_bubble_sort_works_on_simple_array_nums
+    assert_equal [0,1,2,3,4,5,6,7,8,9], bs.bubble_sort([2,0,6,1,8,9,3,7,4,5])
   end
+
+  def test_check_if_bubble_sort_works_on_simple_array_strings
+    assert_equal ["A", "B", "C", "D", "E"], bs.bubble_sort(["E", "D", "C", "B", "A"])
+  end
+
+
 
 
 
