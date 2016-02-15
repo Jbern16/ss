6facdcbc6f3f6b31910d2644e497d25ff91dd738
Jonathan Bernesser <jbern16@gmail.com>
2016-02-15 16:15:57 -0700

    base expectations done - edited insertion

--- lib/insertion_sort_in_place.rb
+++ lib/insertion_sort_in_place.rb
@@ -1,13 +1,14 @@
 class InsertionSortInPlace
 
   def insertion_sort(array)
-  0.upto(array.length-1).each do |index|
-    num_to_insert = array.delete_at(index)
-    insertion_index = index
-    insertion_index -= 1 while insertion_index > 0 && num_to_insert < array[insertion_index -1]
+    0.upto(array.length-1).each do |index|
+      num_to_insert = array.delete_at(index)
+      insertion_index = index
+      while insertion_index > 0 && num_to_insert < array[insertion_index -1]
+        insertion_index -= 1
+      end
     array.insert(insertion_index, num_to_insert)
-   end
-  array
+    end
+    array
   end
-
 end
